a37c600ba88f2fc154265cfb3304253f4c507598
Raúl Diaz Campos <ridiaz2@uc.cl>
2019-04-28 14:21:40 -0400

    archivos en estado inicial

--- Entrega 2/datos.py
+++ Entrega 2/datos.py
@@ -1,19 +1,45 @@
-
 # Datos del modelo
+tablas = dict()
 
 # Lectura de demandas
-demandas = dict()
+demanda_prod = dict()
 
 with open("demanda_productos.csv", "r") as archivo:
-    productos = archivo.readline().strip().split(";")[1:]
+    _productos = archivo.readline().strip().split(";")[1:]
     for linea in archivo.readlines():
         dia = linea.strip().split(";")
-        demandas[dia[0]] = {i: int(j) for i, j in zip(productos, dia[1:])}
+        demanda_prod[dia[0]] = {i: int(j) for i, j in zip(_productos, dia[1:])}
+    tablas["demanda_prod"] = demanda_prod
 
+# Lectura demora de proveedores
+llegada_prov = dict()
+
+with open("llegada_proveedores.csv", "r") as archivo:
+    _proveedores = archivo.readline().strip().split(";")[1:]
+    for linea in archivo.readlines():
+        dia = linea.strip().split(";")
+        llegada_prov[dia[0]] = {i: int(j) for i, j in zip(_proveedores, dia[1:])}
+    tablas["llegada_prov"] = llegada_prov
+
+# Lectura demanda diaria
+personas = dict()
+
+with open("llegada_proveedores.csv", "r") as archivo:
+    archivo.readline()
+    for linea in archivo.readlines():
+        dia = linea.strip().split(";")
+        personas[dia[0]] = dia[1]
+    tablas["personas"] = personas
+
+# Lectura datos adicionales
+with open("datos_adicionales.csv", "r") as archivo:
+    for linea in archivo.readlines():
+        linea = linea.strip().split(";")
+        tablas[linea[0]] = linea[1]
 
 # Lectura de tablas
-with open("tablas.csv") as archivo:
-    tablas = dict()
+
+with open("productos.csv") as archivo:
     linea = archivo.readline().strip().split(";")
     variable = linea[0]
     while variable in ["proteinas", "frutas", "ensaladas"]:
@@ -24,12 +50,27 @@ with open("tablas.csv") as archivo:
         archivo.readline()
         linea = archivo.readline().strip().split(";")
         variable = linea[0]
-    while variable != "listo":
+    numeros = archivo.readline().strip().split(";")[1:]
+    linea = archivo.readline().strip().split(";")
+    variable = linea[0]
+    while variable:
+        tablas[variable] = {i: j for i, j in zip(numeros, linea[1:])}
+        linea = archivo.readline().strip().split(";")
+        variable = linea[0]
+
+with open("proveedores_productos.csv") as archivo:
+    linea = archivo.readline().strip().split(";")
+    variable = linea[0]
+    proveedor = []
+    while variable:
         numeros = archivo.readline().strip().split(";")[1:]
         linea = archivo.readline().strip().split(";")
+
         mini_diccionario = dict()
         while linea[0] != "":
-            mini_diccionario[linea[0]] = {int(i): int(j) for i, j in zip(numeros, linea[1:])}
+            if linea[0] not in proveedor:
+                proveedor.append(linea[0])
+            mini_diccionario[linea[0]] = {i: j for i, j in zip(numeros, linea[1:])}
             linea = archivo.readline().strip().split(";")
         tablas[variable] = mini_diccionario
         linea = archivo.readline().strip().split(";")
@@ -40,22 +81,43 @@ with open("tablas.csv") as archivo:
 proteinas = tablas["proteinas"]
 ensaladas = tablas["ensaladas"]
 frutas = tablas["frutas"]
+vol_prod = tablas["valor_prod"]
+inventario_inicial = tablas["inventario_inicial"]
+porciones_prod = tablas["porciones_prod"]
+
 
 # Dar nombre a productos de variables a exportar
 productos_diccionario = {**proteinas, **ensaladas, **frutas}
 proteinas, ensaladas, frutas = [list(i.keys()) for i in (proteinas, ensaladas, frutas)]
 productos = proteinas + ensaladas + frutas
 
-# Dar nombre a otras tablas
+
+# Dar nombre a tablas de producto/proveedores
 periodo_maduracion = tablas["periodo_maduracion"]
 periodo_vencer = tablas["periodo_maduracion"]
+costo_prod = tablas["costo_prod"]
+valor_prod = tablas["valor_prod"]
+
+# Datos adicionales
+cap_max = tablas["cap_max"]
+presupuesto = tablas["presupuesto"]
+
 
 
 '''
-print(demandas)
 print(productos)
 print(proteinas, ensaladas, frutas)
 print(productos_diccionario)
 print(periodo_maduracion)
 print(periodo_vencer)
+print(cap_max)
+print(costo_prod)
+print(demanda_prod)
+print(presupuesto)
+print(vol_prod)
+print(valor_prod)
+print(llegada_prov)
+print(inventario_inicial)
+print(personas)
+print(porciones_prod)
 '''
